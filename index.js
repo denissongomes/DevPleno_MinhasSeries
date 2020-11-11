87e2e5685cdc631b0b7ef6b378496650f4fee08f
Denisson Gomes <dnsgom@gmail.com>
2020-11-10 21:02:10 -0300

    Initial server configuration

---
+++ index.js
@@ -0,0 +1,36 @@
+//Requiered dependencies
+const express = require('express')
+const app = express()
+const path = require('path')
+const mongoose = require('mongoose')
+const port = process.env.PORT || 3000
+
+//mongoDB
+const mongo = process.env.MONGODB || 'mongodb://localhost/minhas-series'
+
+//mongoose
+mongoose.Promise = global.Promise
+
+//process request body
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended:true }))
+//view engine
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+//assets
+app.use(express.static(path.join(__dirname, 'public')))
+
+//routes
+app.get('/', (req,res) => res.send('OK'))
+
+//Conect DB and Server
+mongoose
+    .connect(mongo,  { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+    app.listen(port, () => {
+     console.log('Listening on port: ' + port)
+    })
+})
+.catch(err => {
+    console.log(err)
+})
