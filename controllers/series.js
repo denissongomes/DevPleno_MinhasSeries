b92b22e3ad04f6c723bfbda1de5605c3fd77c756
Denisson Gomes <dnsgom@gmail.com>
2020-11-11 11:35:02 -0300

    Import model series and send data to index

--- controllers/series.js
+++ controllers/series.js
@@ -1,4 +1,12 @@
-const index = (req, res) => res.render('series/index')
+//importa model series para listar os registros
+const Serie = require('../models/series')
+
+const index = (req, res) => {
+    Serie.find({}, (err,docs) => {
+        res.render('series/index', { series: docs }) 
+    })
+    //
+}
 const nova = (req, res) => res.render('series/nova')
 
 module.exports = {
