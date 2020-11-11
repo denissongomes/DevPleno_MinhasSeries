d4500f1fabf7dfdf5281ec769115d9a228d9f686
Denisson Gomes <dnsgom@gmail.com>
2020-11-11 20:25:20 -0300

    Add try/catch to errors

--- controllers/series.js
+++ controllers/series.js
@@ -10,12 +10,21 @@ const index = async ({ Serie } ,req, res) => {
 }
 const novaProcess = async({ Serie }, req, res) => {
    const series = new Serie(req.body)
+   try{
     await series.save()
     res.redirect('/series')
+   }catch(e){
+     
+    res.render('series/nova', {
+       errors: Object.keys(e.errors)
+    })
+   }
+ 
 }
 
+
 const novaForm = (req, res) => {
-    res.render('series/nova')
+    res.render('series/nova', {errors: []})
 }
 
 const removeOne = async({ Serie }, req, res) => {
@@ -25,15 +34,20 @@ const removeOne = async({ Serie }, req, res) => {
 
  const updateForm = async ({ Serie },req, res) => {
   const serie = await Serie.findOne({_id: req.params.id})
-   res.render('series/editar', { serie, labels })  
+   res.render('series/editar', { serie, labels, errors: [] })  
 }
 
 const updateProcess = async({ Serie }, req, res) => {
     const serie =  await Serie.findOne({_id: req.params.id})
     serie.name = req.body.name
     serie.status =  req.body.status
+   try{
     await  serie.save()    
-    res.redirect('/series')   
+    res.redirect('/series') 
+   }catch(e){
+    res.render('series/editar', { serie, labels, errors: Object.keys(e.errors) })
+   }
+    
 }
 
 module.exports = {
