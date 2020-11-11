60d3e109ab18d6b9121912196d69f35f414a1d0c
Denisson Gomes <dnsgom@gmail.com>
2020-11-11 16:32:55 -0300

    Convert index controller to async/await

--- controllers/series.js
+++ controllers/series.js
@@ -4,12 +4,9 @@ const labels = [
     {id: 'watched', name: 'Assistido', badge: 'badge-success'}
 ]
 
-const index = ({ Serie } ,req, res) => {
- 
-    Serie.find({}, (err,docs) => {
-        res.render('series/index', { series: docs, labels }) 
-    })
-    //
+const index = async ({ Serie } ,req, res) => {
+   const  docs = await Serie.find({})
+        res.render('series/index', { series: docs, labels })    
 }
 const novaProcess = ({ Serie }, req, res) => {
    const series = new Serie(req.body)
