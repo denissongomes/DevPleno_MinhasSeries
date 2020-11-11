964f7948f79b710006d655f788c55087d2fce6b8
Denisson Gomes <dnsgom@gmail.com>
2020-11-11 10:59:40 -0300

    Add route series

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const path = require('path')
 const mongoose = require('mongoose')
 const port = process.env.PORT || 3000
 
-//routes
+//routes modules
 pages = require('./routes/pages')
+series = require('./routes/series')
 
 //mongoDB
 const mongo = process.env.MONGODB || 'mongodb://localhost/minhas-series'
@@ -23,8 +24,9 @@ app.set('views', path.join(__dirname, 'views'))
 //assets
 app.use(express.static(path.join(__dirname, 'public')))
 
-//routes for pages
+//routes middlewares
 app.use('/', pages)
+app.use('/series', series)
  
 
 //Conect DB and Server
