a3ca5707aa7ac7b8e6bf0949bfbd619239ac82a9
Denisson Gomes <dnsgom@gmail.com>
2020-11-11 13:31:41 -0300

    Add router get/post to nova series

--- routes/series.js
+++ routes/series.js
@@ -10,5 +10,7 @@ const models = {
 }
 
 router.get('/', seriesController.index.bind(null, models))
+router.get('/nova',seriesController.novaForm)
+router.post('/nova', seriesController.novaProcess.bind(null, models))
 
 module.exports = router
